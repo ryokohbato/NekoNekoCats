71ae858cfc841b1033215394f49027b50cd193b5
ryokohbato <ryokohbato@gmail.com>
2021-03-10 23:42:50 +0900

    feat: Add toolbar and change app style

--- app/src/main/java/com/example/nekonekocats/MainActivity.java
+++ app/src/main/java/com/example/nekonekocats/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nekonekocats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
