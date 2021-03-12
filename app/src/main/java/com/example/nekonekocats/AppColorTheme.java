e7ca07661d9192b04ee698eafaf79dd7b3bdcc93
ryokohbato <ryokohbato@gmail.com>
2021-03-13 00:26:19 +0900

    refactor: Oops

--- app/src/main/java/com/example/nekonekocats/AppColorTheme.java
+++ app/src/main/java/com/example/nekonekocats/AppColorTheme.java
@@ -9,20 +9,15 @@ public class AppColorTheme {
     // Toggle the color theme of this app
     public static void toggle(Activity activity) {
         int currentNightMode = activity.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        System.out.println(currentNightMode);
         switch (currentNightMode) {
             case Configuration.UI_MODE_NIGHT_YES:
                 // Change to light theme
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                currentNightMode = activity.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-                System.out.println(currentNightMode);
                 break;
             case Configuration.UI_MODE_NIGHT_NO:
             default:
                 // Change to night theme
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                currentNightMode = activity.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-                System.out.println(currentNightMode);
                 break;
         }
     }
